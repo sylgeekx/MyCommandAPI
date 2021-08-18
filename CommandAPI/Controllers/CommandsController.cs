262988c7eb683c4486a0f9dd5e958afdd19fd43e
sylgeek <psguillaume@outlook.com>
2021-08-18 13:57:35 +0400

    added controller and model

---
+++ CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
---
+++ CommandAPI/Models/Commands.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Models
+{
+    public class Commands
+    {
+    }
+}
